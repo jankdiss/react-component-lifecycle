693005ed03301f3d03e7cfc81c96bb2a31629bbf
Janaka Dissanayake <janaka.dissan@gmail.com>
2016-01-20 13:57:32 +1100

    New Comment section for es5

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-"# react-component-lifecycle" 
-
+react-component-lifecycle
 Thanks to :
-Have look on the following sample 
-
---> http://javascript.tutorialhorizon.com/2014/09/13/execution-sequence-of-a-react-components-lifecycle-methods/
+	--> http://javascript.tutorialhorizon.com/2014/09/13/execution-sequence-of-a-react-components-lifecycle-methods/
+	-- > https://dzone.com/refcardz/reactjs-essentials
--- es5.jsx
+++ es5.jsx
@@ -1,78 +1,150 @@
-/**
- * @jsx React.DOM
- */
+var React = require('react');
 
-var React = require('react'),
-    MyReactComponent = React.createClass({
+var ComponentName = React.createClass({
 
-    // The object returned by this method sets the initial value of this.state
+
+    // **************************************************************************************************************//
+    // The displayName string is used in debugging messages. JSX sets this value automatically.
+    // **************************************************************************************************************//
+    displayName:"",
+
+    // **************************************************************************************************************//
+    // Invoked once before the component is mounted. The return value will be used as the initial value of this.state.
+    // **************************************************************************************************************//
     getInitialState: function(){
         return {};
     },
 
-    // The object returned by this method sets the initial value of this.props
-    // If a complex object is returned, it is shared among all component instances      
+    // **************************************************************************************************************//
+    // Invoked once and cached when the class is created. Values in the mapping will be set on this.props.
+    // **************************************************************************************************************//
     getDefaultProps: function(){
         return {};
     },
 
-    // Returns the jsx markup for a component
-    // Inspects this.state and this.props create the markup
-    // Should never update this.state or this.props
+    // **************************************************************************************************************//
+    // The render() function should be pure, meaning that it does not modify the component state. 
+    // It should examine this.props and this.state and return a single child element.
+    // **************************************************************************************************************//
     render: function(){
         return (<div></div>);
     },
 
-    // An array of objects each of which can augment the lifecycle methods
+    // **************************************************************************************************************//
+    // The propTypes object allows you to validate props being passed to your components.
+    // **************************************************************************************************************//
+    propTypes: {},
+
+    // **************************************************************************************************************//
+    // The mixins array allows you to use mixins to share behavior among multiple components.
+    // **************************************************************************************************************//
     mixins: [],
 
-    // Functions that can be invoked on the component without creating instances
-    statics: {
-        aStaticFunction: function(){}
+    // **************************************************************************************************************//
+    // The mixins array allows you to use mixins to share behavior among multiple components.
+    // Functions that can be invoked on the component without creating instances.
+    // **************************************************************************************************************//
+    statics: {},
+
+    // < --------------  Components API ------------------> //
+
+    ComponentNameAPICallWraper : {
+        // **************************************************************************************************************//
+        // Merges nextState with the current state.
+        // data = {}
+        // **************************************************************************************************************//
+        setState:setState(data),
+
+        // **************************************************************************************************************//
+        // Like setState(), but deletes any pre-existing state keys that are not in nextState.
+        // **************************************************************************************************************//
+        replaceState:replaceState(),
+
+        // **************************************************************************************************************//
+        // Call render() on the component, skipping shouldComponentUpdate().
+        // **************************************************************************************************************//
+        forceUpdate:forceUpdate(),
+
+        // **************************************************************************************************************//
+        // Call render() on the component, skipping shouldComponentUpdate().
+        // **************************************************************************************************************//
+        findDOMNode: React.findDOMNode(node),
+
+        // **************************************************************************************************************//
+        // isMounted() returns true if the component is rendered into the DOM.
+        // **************************************************************************************************************//
+        isMounted:isMounted(),
+
+        // **************************************************************************************************************//
+        // To change the properties and trigger re-render
+        // data = {}
+        // **************************************************************************************************************//
+        setProps:setProps(data),
+
+        // **************************************************************************************************************//
+        // Like setProps() but deletes any pre-existing props instead of merging the two objects.
+        // data = {}
+        // func =   function
+        // **************************************************************************************************************//
+        replaceProps:replaceProps(data,func)
     },
 
-    // -- Lifecycle Methods --
 
-    // Invoked once before first render
+    // < --------------  Lifecycle Methods ------------------> //
+
+    // **************************************************************************************************************//
+    // Invoked once, both on the client and server, immediately before the initial rendering occurs.
+    // **************************************************************************************************************//
     componentWillMount: function(){
         // Calling setState here does not cause a re-render
     },
 
-    // Invoked once after the first render
+    // **************************************************************************************************************//
+    // Invoked on the client immediately after the initial rendering occurs.
+    // **************************************************************************************************************//
     componentDidMount: function(){
         // You now have access to this.getDOMNode()
     },
 
-    // Invoked whenever there is a prop change
+    // **************************************************************************************************************//
+    // Invoked when a component is receiving new prop.
     // Called BEFORE render
+    // **************************************************************************************************************//
     componentWillReceiveProps: function(nextProps){
         // Not called for the initial render
         // Previous props can be accessed by this.props
-        // Calling setState here does not trigger an an additional re-render
+        // Calling setState here does not trigger an an additional re-render**
     },
 
-    // Determines if the render method should run in the subsequent step
-    // Called BEFORE a render
-    // Not called for the initial render
+    // **************************************************************************************************************//
+    // Invoked before rendering when new props or state are being received. Skips render() if returns false.
+    // Called BEFORE render
+    // **************************************************************************************************************//
     shouldComponentUpdate: function(nextProps, nextState){
         // If you want the render method to execute in the next step
         // return true, else return false
         return true;
     },
 
-    // Called IMMEDIATELY BEFORE a render
+    // **************************************************************************************************************//
+    // Invoked immediately before rendering when new props or state are being received.
+    // **************************************************************************************************************//
     componentWillUpdate: function(nextProps, nextState){
         // You cannot use this.setState() in this method
     },
 
-    // Called IMMEDIATELY AFTER a render
+    // **************************************************************************************************************//
+    // Invoked immediately after the component's updates are flushed to the DOM. Operate on the DOM here.
+    // **************************************************************************************************************//
     componentDidUpdate: function(prevProps, prevState){
     },
 
-    // Called IMMEDIATELY before a component is unmounted
+    // **************************************************************************************************************//
+    // Invoked immediately before a component is unmounted from the DOM.
+    // **************************************************************************************************************//
     componentWillUnmount: function(){
     }
 
 });
 
-module.exports = MyReactComponent;
+module.exports = ComponentName;
